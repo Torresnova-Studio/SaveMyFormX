989442fa25aca55e0ba3ddfc3774ff15494a619a
Torresnova-Studio <torresnovastudio@gmail.com>
2025-04-14 17:18:51 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,6 +1,15 @@
-document.getElementById("clear").addEventListener("click", () => {
-    chrome.storage.local.clear(() => {
-      alert("Datos eliminados correctamente.");
-    });
+// Inyectar el content script en la pestaña activa
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  const tab = tabs[0];
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ["content.js"]
   });
-  
+});
+
+// Botón para borrar datos guardados
+document.getElementById("clear").addEventListener("click", () => {
+  chrome.storage.local.clear(() => {
+    alert("Datos eliminados correctamente.");
+  });
+});
