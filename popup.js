5495256991904eeb903012f16c5125c489209847
Torresnova-Studio <torresnovastudio@gmail.com>
2025-04-14 18:53:47 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,12 +1,3 @@
-// Inyectar el content script en la pestaña activa
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  const tab = tabs[0];
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ["content.js"]
-  });
-});
-
 // Botón para borrar datos guardados
 document.getElementById("clear").addEventListener("click", () => {
   chrome.storage.local.clear(() => {
